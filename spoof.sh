7fd9c8109821ab007cc630aac46d6e0f7aa69799
DNZ15 <dennis_joosens@hotmail.com>
2017-11-02 22:07:08 +0100

    1st commit

---
+++ spoof.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#prerequisites:
+#sudo apt install macchanger
+
+echo ".....WELCOME TO SPOOFY....."
+echo ".....NOTE: MAKE SURE YOU HAVE SUDO PRIVILEGES....."
+#sudo su
+ifconfig wlo1 down
+macchanger -r wlo1
+ifconfig wlo1 up
+
+echo ".....New MAC ADDRESS APPLIED....."
+
+echo ".....RElEASING IP ADDRESS....."
+ifconfig -a | grep 'inet addr:'
+dhclient -r
+
+echo ".....RENEWING IP ADDRESS....."
+dhclient
+sleep 3
+ifconfig -a | grep 'inet addr:'
+
+echo ".....OK....."
